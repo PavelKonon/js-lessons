d29fa3ad9b48c65f90da38877a5ae2d69fc571d0
pavel <konon.pavel@gmail.com>
2016-05-12 18:49:12 +0300

    - Quickfix

--- lesson-1/jamapi/example-1.js
+++ lesson-1/jamapi/example-1.js
@@ -1,4 +1,9 @@
 const request = require('request');
-request.post('http://www.jamapi.xyz/', {form: {url: 'http://p97.com', json_data: '{"title": "title"}'}}, function(err, response, body) {
+request.post('http://www.jamapi.xyz/', {
+    form: {
+        url: 'http://p97.com',
+        json_data: '{"title": "title"}'
+    }
+}, function(err, response, body) {
     console.log(body);
 })
